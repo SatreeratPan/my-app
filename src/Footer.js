2133cebdf8444e4886529268a6523e08edaa4bdc
SatreeratPan <satreerat.pa.65@ubu.ac.th>
2023-06-27 23:03:46 +0700

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Footer() {
+    return (
+        <div className='MyFooter'>
+            <span>2023 @satreerat panyawan</span>
+        </div>
+    );
+}
+
+export default Footer;
